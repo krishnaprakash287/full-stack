46dddf7ac677254e35c966417299681a98885b0e
krishnaprakash287 <kp13226663@gmail.com>
2024-11-12 21:23:00 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://full-stack-backend-uxba.onrender.com",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
